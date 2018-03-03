013386ce6b4120116c1d373620aa77c664ecd53b
Pavel Gordon <p.gordon@interanw.ru>
2018-03-03 11:16:46 +0100

    Implement functionality of encoding/decoding

---
+++ src/main/java/com/freemahn/MainWindow.java
@@ -0,0 +1,204 @@
+package com.freemahn;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainWindow
+        extends Application {
+
+    private Text actionStatus;
+    private Stage savedStage;
+    private Stage stage;
+    private Button executeBtn;
+    private TextArea txtArea;
+    private static final String titleTxt = "Tiny Url Encoder-Decoder";
+    private static final String defaultFileName = "output.txt";
+
+    private File inputFile, outputFile;
+    private List<String> encodedUrls;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle(titleTxt);
+
+        // Window label
+        Label label = new Label("Tiny Url Encoder-Decoder");
+        label.setTextFill(Color.DARKBLUE);
+        label.setFont(Font.font("Calibri", FontWeight.BOLD, 36));
+        HBox labelHb = new HBox();
+        labelHb.setAlignment(Pos.CENTER);
+        labelHb.getChildren().add(label);
+
+        // Text area in a scrollpane and label
+        Label txtAreaLabel = new Label("Input file text:");
+        txtAreaLabel.setFont(Font.font("Calibri", FontWeight.NORMAL, 20));
+        txtArea = new TextArea();
+        txtArea.setWrapText(true);
+        txtArea.setDisable(true);
+
+        ScrollPane scroll = new ScrollPane();
+        scroll.setContent(txtArea);
+        scroll.setFitToWidth(true);
+        scroll.setFitToHeight(true);
+        scroll.setPrefHeight(150);
+        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+        VBox txtAreaVbox = new VBox(5);
+        txtAreaVbox.setPadding(new Insets(5, 5, 5, 5));
+        txtAreaVbox.getChildren().addAll(txtAreaLabel, scroll);
+        // Button
+        Button openInputFile = new Button("Choose input file(TXT)...");
+        openInputFile.setOnAction(new OpenFileButtonListener());
+
+        Button saveOutputFile = new Button("Choose output file(TXT)...");
+        saveOutputFile.setOnAction(new SaveButtonListener());
+
+        executeBtn = new Button("Execute parsing");
+        executeBtn.setDisable(true);
+        executeBtn.setOnAction(new ExecuteListener());
+
+        HBox buttonHb1 = new HBox(10);
+        buttonHb1.setAlignment(Pos.CENTER);
+        buttonHb1.getChildren().addAll(openInputFile, saveOutputFile, executeBtn);
+
+        // Status message text
+        actionStatus = new Text();
+        actionStatus.setFont(Font.font("Calibri", FontWeight.NORMAL, 20));
+        actionStatus.setFill(Color.FIREBRICK);
+        actionStatus.setText("Please choose input file, output file, and then execute");
+
+        // Vbox
+        VBox vbox = new VBox(30);
+        vbox.setPadding(new Insets(25, 25, 25, 25));
+        vbox.getChildren().addAll(labelHb, txtAreaVbox, buttonHb1, actionStatus);
+
+        // Scene
+        Scene scene = new Scene(vbox, 600, 400); // w x h
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        savedStage = primaryStage;
+    }
+
+    private class OpenFileButtonListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent e) {
+            FileChooser fileChooser = new FileChooser();
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+            fileChooser.getExtensionFilters().add(extFilter);
+            fileChooser.setTitle("Load input file");
+            inputFile = fileChooser.showOpenDialog(stage);
+            if (inputFile != null) {
+                encodedUrls = readFile(inputFile);
+                txtArea.setText(String.join("\n", encodedUrls));
+                txtArea.setDisable(false);
+            }
+
+        }
+    }
+
+    private class SaveButtonListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent e) {
+            FileChooser fileChooser = new FileChooser();
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+            fileChooser.getExtensionFilters().add(extFilter);
+            fileChooser.setTitle("Save file");
+            fileChooser.setInitialFileName(defaultFileName);
+            outputFile = fileChooser.showSaveDialog(savedStage);
+            if (outputFile != null && inputFile != null) {
+                executeBtn.setDisable(false);
+            }
+        }
+    }
+
+    private class ExecuteListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent e) {
+            if (encodedUrls == null)
+                return;
+            List<String> parsedResults = new ArrayList<>();
+            if (encodedUrls.get(0).startsWith("/x/"))
+                parsedResults = Parser.tinyUrl2Id(encodedUrls);
+            else if (isNumeric(encodedUrls.get(0))) {
+                parsedResults = Parser.id2TinyUrl(encodedUrls);
+            } else {
+                actionStatus.setText("Wrong input file format, should be /x/ChDH or 13045770");
+            }
+            writeFile(parsedResults);
+
+        }
+    }
+
+
+    private List<String> readFile(File file) {
+        List<String> stringList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = br.readLine();
+            while (line != null) {
+                stringList.add(line);
+                line = br.readLine();
+            }
+            actionStatus.setText("File " +
+                    inputFile.toString() + " successfully read");
+
+        } catch (IOException e) {
+            actionStatus.setText("An ERROR occurred while opening input file!" +
+                    inputFile.toString());
+        }
+        return stringList;
+    }
+
+    private void writeFile(List<String> parsedIds) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String s : parsedIds) {
+                writer.write(s + "\n");
+            }
+            actionStatus.setText("File " +
+                    outputFile.toString() + " successfully written!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            actionStatus.setText("An ERROR occurred while saving the file!" +
+                    outputFile.toString());
+        }
+
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            int d = Integer.parseInt(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/freemahn/Parser.java
@@ -0,0 +1,92 @@
+package com.freemahn;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+
+
+//    public static void main(String[] args) {
+//        System.out.println(id2TinyUrl("13045735"));
+//        System.out.println(tinyUrl2Id("/x/5w-H"));
+//    }
+
+    public static List<String> tinyUrl2Id(List<String> list) {
+        return list.stream().map(Parser::tinyUrl2Id).collect(Collectors.toList());
+    }
+
+    public static List<String> id2TinyUrl(List<String> list) {
+        return list.stream().map(Parser::id2TinyUrl).collect(Collectors.toList());
+    }
+
+    private static String id2TinyUrl(String id) {
+        String result = encode(Integer.parseInt(id));
+        return id + "," + result;
+
+    }
+
+    private static String encode(Integer id) {
+        byte[] bytes = ByteBuffer.allocate(4).putInt(id).array();
+
+        for(int i = 0; i < bytes.length / 2; i++)
+        {
+            byte temp = bytes[i];
+            bytes[i] = bytes[bytes.length - i - 1];
+            bytes[bytes.length - i - 1] = temp;
+        }
+
+        String result = Base64.getEncoder().encodeToString(bytes);
+        result = result.replace("+", "_").replace("/", "-");
+        result = "/x/" + result.split("AA==")[0];
+        return result;
+    }
+
+    private static String tinyUrl2Id(String url) {
+        String t = url.substring(3).replace("_", "+").replace("-", "/");
+        String result = "";
+        try {
+            result = decode(t);
+        } catch (Exception e) {
+            System.err.println(url);
+        }
+        return url + "," + result;
+    }
+
+
+    private static String decode(String url) {
+        url = padRight(url, 8).replace(' ', 'A');
+        byte[] bytes = Base64.getDecoder().decode(url);
+        System.out.println(Arrays.toString(bytes));
+        ByteBuffer byteBuf = ByteBuffer.wrap(bytes);
+
+        // then turn it into an IntBuffer, using big-endian ("Network") byte order:
+        byteBuf.order(ByteOrder.LITTLE_ENDIAN);
+        IntBuffer intBuf = byteBuf.asIntBuffer();
+//        System.out.println(Arrays.toString(byteBuf.array()));
+        // finally, dump the contents of the IntBuffer into an array
+        int[] integers = new int[intBuf.remaining()];
+        intBuf.get(integers);
+        return String.valueOf(integers[0]);
+
+    }
+
+    private static String padRight(String s, int n) {
+        return String.format("%1$-" + n + "s", s);
+    }
+
+
+
+    public static byte[] stringToBytes(final String input) {
+        // unlike Stirng.toByteArray(), we ignore any high-byte values of the characters.
+        byte[] ret = new byte[input.length()];
+        for (int i = input.length() - 1; i >=0; i--) {
+            ret[i] = (byte)input.charAt(i);
+        }
+        return ret;
+    }
+}
